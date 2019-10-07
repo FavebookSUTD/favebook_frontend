dd51033f69f82794fa34d57a3d2a6d005eb709b1
TAY TS <taytzushieh@gmail.com>
2019-10-08 00:07:47 +0800

    Update styling for submenu

--- src/pages/HomePage/components/HeaderMenu/index.jsx
+++ src/pages/HomePage/components/HeaderMenu/index.jsx
@@ -19,7 +19,7 @@ const HeaderMenu = ({ menuClickHandler }) => {
         <Menu.Item key="home">HOME</Menu.Item>
         <Menu.Item key="mybooks">MY BOOKS</Menu.Item>
         <SubMenu
-          className="submenu__container"
+          popupClassName="home-page-submenu__container"
           title={
             <>
               <span className="submenu-title__container">BROWSE</span>
@@ -27,8 +27,42 @@ const HeaderMenu = ({ menuClickHandler }) => {
             </>
           }
         >
-          <Menu.Item key="option1">Option 1</Menu.Item>
-          <Menu.Item key="option2">Option 2</Menu.Item>
+          <Menu.ItemGroup className="menu-item-group__main-container">
+            <Menu.Item key="recommendations">Recommendations</Menu.Item>
+            <Menu.Item key="choice-awards">Choice Awards</Menu.Item>
+            <Menu.Item key="giveaways">Giveaways</Menu.Item>
+            <Menu.Item key="new-releases">New Releases</Menu.Item>
+            <Menu.Item key="news-feed">News Feed</Menu.Item>
+          </Menu.ItemGroup>
+          <Menu.ItemGroup className="menu-item-group__genres-container" title="GENRES">
+            <Menu.Item key="art">Art</Menu.Item>
+            <Menu.Item key="comics">Comics</Menu.Item>
+            <Menu.Item key="historical-fiction">Historical Fiction</Menu.Item>
+            <Menu.Item key="psychology">Psychology</Menu.Item>
+            <Menu.Item key="thriller">Thriller</Menu.Item>
+            <Menu.Item key="biography">Biography</Menu.Item>
+            <Menu.Item key="cookbooks">Cookbooks</Menu.Item>
+            <Menu.Item key="history">History</Menu.Item>
+            <Menu.Item key="romance">Romance</Menu.Item>
+            <Menu.Item key="travel">Travel</Menu.Item>
+            <Menu.Item key="business">Business</Menu.Item>
+            <Menu.Item key="ebooks">Ebooks</Menu.Item>
+            <Menu.Item key="horror">Hooror</Menu.Item>
+            <Menu.Item key="science">Science</Menu.Item>
+            <Menu.Item key="young-adult">Young Adult</Menu.Item>
+            <Menu.Item key="children">Children's</Menu.Item>
+            <Menu.Item key="fantasy">Fantasy</Menu.Item>
+            <Menu.Item key="memoir">Memoir</Menu.Item>
+            <Menu.Item key="science-fiction">Science Fiction</Menu.Item>
+            <Menu.Item key="christian">Christian</Menu.Item>
+            <Menu.Item key="fiction">Fiction</Menu.Item>
+            <Menu.Item key="music">Music</Menu.Item>
+            <Menu.Item key="self-help">Self Help</Menu.Item>
+            <Menu.Item key="classic">Classic</Menu.Item>
+            <Menu.Item key="graphic-novels">Graphic Novels</Menu.Item>
+            <Menu.Item key="mystery">Mystery</Menu.Item>
+            <Menu.Item key="sports">Sports</Menu.Item>
+          </Menu.ItemGroup>
         </SubMenu>
         <Menu.Item key="newsfeed">NEWSFEED</Menu.Item>
       </Menu>
