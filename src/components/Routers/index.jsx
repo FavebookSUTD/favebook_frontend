5233827c587283f7f5d2363b4a7f23cff3aea90b
Eda <edathr2@gmail.com>
2019-10-08 11:30:48 +0800

    Revert "Created BrowseResultsPage"
    This reverts commit 51475668a6d5524be6b6296f8f08b4a0117c1370.

--- src/components/Routers/index.jsx
+++ src/components/Routers/index.jsx
@@ -5,14 +5,12 @@ import { Switch, Route } from 'react-router-dom';
 // import page component
 import AuthenticatePage from '@pages/AuthenticatePage';
 import HomePage from '@pages/HomePage';
-import BrowseResultsPage from '@pages/BrowseResultsPage';
 
 const Routers = () => {
   return (
     <Switch>
       <Route exact path="/" render={() => <HomePage />} />
       <Route exact path="/authenticate" render={() => <AuthenticatePage />} />
-      <Route exact path="/browseresults" render={() => <BrowseResultsPage />} />
     </Switch>
   );
 };
