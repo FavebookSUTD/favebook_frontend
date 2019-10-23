c5442ac850ef0d9967d5d34c47117c1bf7bab4be
Eda <edathr2@gmail.com>
2019-10-23 22:25:39 +0800

    Changed Select to Autocomplete
    - Changed design of items
    - Redirect users to BrowseResultsPage when 'filter' button is clicked

--- src/containers/FilterBar/index.jsx
+++ src/containers/FilterBar/index.jsx
@@ -21,20 +21,29 @@ import { selectError, selectFetching, selectSearchResult } from './selectors';
 import debounce from 'lodash/debounce';
 import map from 'lodash/map';
 
+// import utils
+import { goto } from '@utils/goto';
+
 // import local styling
 import './index.scss';
 
 // import Antd
-import { Icon, Button, Select, Spin } from 'antd';
+import { Icon, Button, Spin, AutoComplete, Typography } from 'antd';
 
 // Extract antd components
-const { Option } = Select;
+const { Option } = AutoComplete;
+const { Text } = Typography;
 
 const renderOptions = options =>
   map(options, option => (
-    <Option className="search-result__option" key={option.email} label={option.email}>
+    <Option className="search-result__option" key={option.title} label={option.title}>
       <Icon className="search-result-icon" type="search" />
-      {option.email}
+      <img className="book-cover-img" src={option.imgURL} alt="Book" />
+      <div className="search-result-info">
+        {option.title}
+        <br />
+        <Text className="search-result-info-author">{option.author}</Text>
+      </div>
     </Option>
   ));
 
@@ -55,29 +64,29 @@ class FilterBar extends PureComponent {
 
     return (
       <div className={`filter-bar ${position}`}>
-        <Select
-          className="filter-select__container"
-          showSearch
+        <AutoComplete
+          className="filter-autocomplete__container"
           showArrow={false}
           filterOption={false}
           defaultActiveFirstOption={false}
           notFoundContent={fetching && searchVal ? <Spin className="filter-spining-icon" /> : null}
           optionLabelProp="label"
           placeholder={<Icon className="search-icon" type="search" />}
-          dropdownClassName="filter-select-dropdown__container"
+          dropdownClassName="filter-autocomplete-dropdown__container"
           onDropdownVisibleChange={open => this.setState({ dropdownOpened: open })}
+          dataSource={searchResults}
           onSearch={val => {
             this.setState({ searchVal: val });
             this.deboucedSearchBooks(val);
           }}
         >
           {renderOptions(searchResults)}
-        </Select>
+        </AutoComplete>
         <Button
           className="filter-button"
           type="primary"
           disabled={dropdownOpened}
-          onClick={() => console.log('Filter')}
+          onClick={() => goto('/browseresults')}
         >
           FILTER
         </Button>
