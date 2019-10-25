76944b0dce9cbf85a47d3a5bbed8668b03e14f80
TAY TS <taytzushieh@gmail.com>
2019-10-26 03:28:48 +0800

    Add loading state handling for BookDescriptions component

--- src/pages/BookDetailsPage/components/BookDescriptions/index.jsx
+++ src/pages/BookDetailsPage/components/BookDescriptions/index.jsx
@@ -12,57 +12,81 @@ import map from 'lodash/map';
 import './index.scss';
 
 // import Antd
-import { Typography, Icon, Button, Tag } from 'antd';
+import { Typography, Icon, Button, Tag, Skeleton } from 'antd';
 
 // Extract antd components
 const { Title, Text } = Typography;
 
-const BookDescriptions = ({ title, author, ratingValue, commentCount, purchaseLinks, genres }) => {
+const BookDescriptions = ({
+  loading,
+  title,
+  author,
+  ratingValue,
+  commentCount,
+  purchaseLinks,
+  genres,
+}) => {
   return (
     <div className="book-descriptions__main-container">
-      <div className="book-description__basic-details">
-        <Title className="book-descriptions__title" level={3}>
-          {title}
-        </Title>
-        <Text className="book-descriptions__author" ellipsis>
-          {author}
-        </Text>
-      </div>
-      <div className="book-rating__container">
-        <BookRatingStar ratingValue={ratingValue} />
-        <Text className="review-count" strong>
-          <Icon className="comment-icon" type="message" />
-          {`${commentCount}`}
-        </Text>
-      </div>
-      <div className="book-purchase__btn-group">
-        <Text className="book-purchase__title" strong>
-          Buy on
-        </Text>
-        <Button className="book-purchase__btn" size="large" type="primary">
-          AMAZON
-        </Button>
-        <Button className="book-purchase__btn" size="large" type="primary">
-          KINDLE
-        </Button>
-      </div>
-      <div className="book-genre-tags__container">
-        {map(genres, genre => (
-          <Tag key={genre} className="book-genre-tag">
-            {genre}
-          </Tag>
-        ))}
-      </div>
+      <Skeleton loading={loading} active>
+        <div className="book-description__basic-details">
+          <Title className="book-descriptions__title" level={3}>
+            {title}
+          </Title>
+          <Text className="book-descriptions__author" ellipsis>
+            {author}
+          </Text>
+        </div>
+        <div className="book-rating__container">
+          <BookRatingStar ratingValue={ratingValue} />
+          <Text className="review-count" strong>
+            <Icon className="comment-icon" type="message" />
+            {`${commentCount}`}
+          </Text>
+        </div>
+        <div className="book-purchase__btn-group">
+          <Text className="book-purchase__title" strong>
+            Buy on
+          </Text>
+          <Button
+            className="book-purchase__btn"
+            disabled={!purchaseLinks || !purchaseLinks.amazon}
+            size="large"
+            type="primary"
+          >
+            AMAZON
+          </Button>
+          <Button
+            className="book-purchase__btn"
+            disabled={!purchaseLinks || !purchaseLinks.kindle}
+            size="large"
+            type="primary"
+          >
+            KINDLE
+          </Button>
+        </div>
+        <div className="book-genre-tags__container">
+          {map(genres, genre => (
+            <Tag key={genre} className="book-genre-tag">
+              {genre}
+            </Tag>
+          ))}
+        </div>
+      </Skeleton>
     </div>
   );
 };
 
 BookDescriptions.propTypes = {
+  loading: PropTypes.bool.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
   ratingValue: PropTypes.number.isRequired,
   commentCount: PropTypes.number.isRequired,
-  purchaseLinks: PropTypes.shape({}).isRequired,
+  purchaseLinks: PropTypes.shape({
+    amazon: PropTypes.string,
+    kindle: PropTypes.string,
+  }).isRequired,
   genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
