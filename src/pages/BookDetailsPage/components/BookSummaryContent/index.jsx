29c751355f65f7eb41de0ba51fd2ec6a99955a1f
TAY TS <taytzushieh@gmail.com>
2019-10-26 02:48:20 +0800

    Add loading state for BookSummaryContent and BookDetailsContent

--- src/pages/BookDetailsPage/components/BookSummaryContent/index.jsx
+++ src/pages/BookDetailsPage/components/BookSummaryContent/index.jsx
@@ -6,21 +6,24 @@ import PropTypes from 'prop-types';
 import './index.scss';
 
 // import Antd
-import { Typography, Empty } from 'antd';
+import { Typography, Empty, Skeleton } from 'antd';
 
 // Extract antd components
 const { Paragraph } = Typography;
 
-const BookSummaryContent = ({ summary }) => {
+const BookSummaryContent = ({ summary, loading }) => {
   return (
     <div className="book-summary__content-container">
-      {summary ? <Paragraph className="book-summary-content">{summary}</Paragraph> : <Empty />}
+      <Skeleton loading={loading} active>
+        {summary ? <Paragraph className="book-summary-content">{summary}</Paragraph> : <Empty />}
+      </Skeleton>
     </div>
   );
 };
 
 BookSummaryContent.propTypes = {
   summary: PropTypes.string,
+  loading: PropTypes.bool.isRequired,
 };
 
 BookSummaryContent.defaultProps = {
